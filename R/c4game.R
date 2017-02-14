9e527baacf2a06d703f958486a20c4e43af5695f
Mark Sellors <github@5vcc.com>
2017-02-14 21:40:34 +0000

    Tidied things up a little

--- R/c4game.R
+++ R/c4game.R
@@ -7,36 +7,48 @@
 #' @examples
 #' c4game$new(board = matrix(, nrow = 6, ncol = 7), player = 1)
 #' returns an r5 object
-c4game <- setRefClass("c4game",
-                      fields = list(board = "matrix",
-                                    gamestate = "character",
-                                    player = "numeric"),
-                      methods = list(
-                        dropToken = function(colnum){
-                          # need the height of the board in case a non-standard size is specified
-                          boardrows <- c(length(board[,1]):1)
-                          # Check each row in reverse to see if it's empty
-                          for(boardrow in boardrows){
-                            # Place the token in the first empty slot
-                            if( is.na(board[boardrow, colnum]) ){
-                              board[boardrow, colnum] <<- player
-                              # check all directions through the placed token for 4 in a row.
-                              if( any(rle(board[,colnum])$lengths >= 4) ||
-                                  any(rle(board[boardrow,])$lengths >= 4) ||
-                                  any(rle(board[row(board) - col(board) == colnum - boardrow ])$lengths >= 4) ||
-                                  any(rle(board[row(board) + col(board) == colnum + boardrow ])$lengths >= 4)){
-                                # update the gamestate with player winner
-                                gamestate <<- paste("player", player, "wins")
-                                player <<- 0
-                              } else {
-                                gamestate <<- "next"
-                                if ( player == 1 ){ player <<- 2 } else { player <<- 1 }
-                              }
-                              break
-                            }
-                          }
-                        },
-                        setPlayer = function(x){ player <<- x },
-                        setState = function(x){ gamestate <<- x }
-                      )
+c4game <- setRefClass(
+  "c4game",
+  fields = list(
+    board = "matrix",
+    gamestate = "character",
+    player = "numeric"
+  ),
+  methods = list(
+    dropToken = function(colnum) {
+      # need the height of the board in case a non-standard size is specified
+      boardrows <- c(length(board[, 1]):1)
+      # Check each row in reverse to see if it's empty
+      for (boardrow in boardrows) {
+        # Place the token in the first empty slot
+        if (is.na(board[boardrow, colnum])) {
+          board[boardrow, colnum] <<- player
+          # check all directions through the placed token for 4 in a row.
+          if (any(rle(board[, colnum])$lengths >= 4) ||
+              any(rle(board[boardrow, ])$lengths >= 4) ||
+              any(rle(board[row(board) - col(board) == colnum - boardrow])$lengths >= 4) ||
+              any(rle(board[row(board) + col(board) == colnum + boardrow])$lengths >= 4)) {
+            # update the gamestate with player winner
+            gamestate <<-
+              paste("player", player, "wins")
+            player <<- 0
+          } else {
+            gamestate <<- "next"
+            if (player == 1) {
+              player <<- 2
+            } else {
+              player <<- 1
+            }
+          }
+          break
+        }
+      }
+    },
+    setPlayer = function(x) {
+      player <<- x
+    },
+    setState = function(x) {
+      gamestate <<- x
+    }
+  )
 )
