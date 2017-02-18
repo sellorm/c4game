c8543669bd3ed99eec083317fe0ee6a2f67aae71
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-02-18 11:11:50 +0000

    always win

--- tests/testthat/test-challenger.R
+++ tests/testthat/test-challenger.R
@@ -5,20 +5,25 @@ context("Beat the champion!")
 
 test_that("The champion is the best!", {
 
-  champion <- sequencePlayer$new(rep(1:7, 6))
-  challenger <- randomPlayer$new()
+  set.seed(14)
 
   ngames <- 11
   results <- integer(length = ngames)
 
   starter <- sample(1:2, size = ngames, replace = TRUE)
   for (i in seq.int(ngames)) {
+
+    champion <- sequencePlayer$new(rep(1:7, 6))
+    challenger <- sequencePlayer$new(sample(1:7, size = 100, replace = TRUE))
+
     results[i] <- c4arena(champion, challenger, firstplayer = starter[i])
   }
 
   tally <- table(results)
+  cat(tally, fill = TRUE)
+  cat(results, fill = TRUE)
 
-  expect_gt(tally[1], tally[2], message = "You didn't win this time. Dust yourself off and go again!")
+  expect_gt(tally[1], tally[2], label = "You didn't win this time. Dust yourself off and go again!")
 
 })
 
