a3d28a2a25436e1c6fa3647590931a353613b572
Mark Sellors <github@5vcc.com>
2017-02-18 00:38:20 +0000

    removed playRandomGame, but kept c4repl. c4replis now a simple wrapper around c4arena that sets up 3 simple types of game with either 0, 1 or 2 human players. Closes #8

--- R/c4repl.R
+++ R/c4repl.R
@@ -1,22 +1,22 @@
 #' Starts a simple REPL to play connect 4.
+#' A wrapper for c4arena, c4repl allows you to play against R or a friend.
 #'
+#' @param players The number of human players, either 0, 1 or 2. default is 1.
 #' @export
 #' @examples
-#' \dontrun{
-#' c4repl()
-#' }
-c4repl <- function(){
-  # initialise the game
-  game <- c4game$new(board = matrix(NA, nrow = 6, ncol = 7), player = 1, gamestate = "next")
-  print(game$board)
-  # start the game
-  while(TRUE){
-    c4prompt <- paste0("Player", game$player, "> ")
-    userInput <- readline(c4prompt)
-    if(userInput == "q"){break}
-    game$dropToken(userInput)
-    print(game$board)
-    print(game$gamestate)
-    if(game$gamestate != "next"){break}
+#' c4repl(0)
+c4repl <- function(players = 1){
+  if ( players == 2 ) {
+    p1 <- human$new(name="1")
+    p2 <- human$new(name="2")
+  } else if ( players == 1 ){
+    p1 <- human$new(name="1")
+    p2 = randomPlayer$new(name="2")
+  } else if ( players == 0 ){
+    p1 = randomPlayer$new(name="1")
+    p2 = randomPlayer$new(name="2")
+  } else {
+    return("Error: number of players must be 0, 1 or 2")
   }
+  c4arena(p1, p2)
 }
