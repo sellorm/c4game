82c9d1f76ee942f7f5b63d4836e9ec3ff63930f1
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-02-19 21:44:34 +0000

    increase the challenge to 101 games

--- tests/testthat/test-challenger.R
+++ tests/testthat/test-challenger.R
@@ -7,7 +7,7 @@ test_that("The champion is the best!", {
 
   set.seed(14)
 
-  ngames <- 11
+  ngames <- 101
   results <- integer(length = ngames)
 
   starter <- sample(1:2, size = ngames, replace = TRUE)
@@ -20,8 +20,7 @@ test_that("The champion is the best!", {
   }
 
   tally <- table(results)
-  cat(tally, fill = TRUE)
-  cat(results, fill = TRUE)
+  print(tally)
 
   expect_gt(tally["1"], tally["2"], label = "You didn't win this time. Dust yourself off and go again!")
 
